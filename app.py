4c81c47de6169f41e9eacbd6a235072aa0df0f55
Manaswee Mandal <mandalmanaswee@gmail.com>
2026-02-03 02:34:59 +0530

    Fix

--- app.py
+++ app.py
@@ -70,9 +70,7 @@ def user_input(user_question):
     )
     docs = new_db.similarity_search(user_question)
     chain = get_conversational_chain()
-    response = chain(
-        {"input_documents": docs, "question": user_question}, return_only_outputs=True
-    )
+    response = chain.invoke({"input_documents": docs, "question": user_question})
     st.write("Reply: ", response["output_text"])
 
 
