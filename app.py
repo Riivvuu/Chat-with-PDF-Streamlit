d2bbf99178ca103e5fd20eb5b3a4fcddc4e450e3
Manaswee Mandal <mandalmanaswee@gmail.com>
2026-02-04 13:07:37 +0530

    switching gemini model

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def get_conversational_chain():
     Question: \n{question}\n
     Answer:
     """
-    model = ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature=0.3)
+    model = ChatGoogleGenerativeAI(model="gemini-2.0-flash", temperature=0.3)
     prompt = PromptTemplate(
         template=prompt_template, input_variables=["context", "question"]
     )
